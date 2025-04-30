1edb8222a8814b1d66dbafaf20fc2563560327c5
제동균 <donggyunje@gmail.com>
2025-04-30 21:09:54 +0900

    Feature : 공통 타이틀 제작 S12P31S108-133

---
+++ FE/src/components/common/Title.tsx
@@ -0,0 +1,19 @@
+interface TitleProps {
+  text: string;
+  subText: string;
+}
+
+function Title({ text, subText }: TitleProps) {
+  return (
+    <div className="flex flex-col p-8">
+      <div className="font-pretendard text-primary-black text-title">
+        {text}
+      </div>
+      <div className="font-samsung400 text-primary-gray text-subtitle">
+        {subText}
+      </div>
+    </div>
+  );
+}
+
+export default Title;
