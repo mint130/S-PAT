475b97db1c5b00c13c8f28ec5d3a354c7ff8b7b8
김민주 <mintminju130@naver.com>
2025-05-01 23:22:26 +0900

    Setting: Dockerfile에 alembic, health check 추가
    S12P31S108-138

--- BE/app/main.py
+++ BE/app/main.py
@@ -23,6 +23,9 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+@app.get("/health")
+async def health_check():
+    return {"status": "healthy"}
 
 if __name__ == "__main__":
     import uvicorn
