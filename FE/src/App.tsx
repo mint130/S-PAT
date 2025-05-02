28546a29abafd70448168d6d27997dc138ef771c
이송희 <thdgml1103@gmail.com>
2025-05-02 10:46:44 +0900

    Feat: 모드와 스텝에 따른 페이지 구분

--- FE/src/App.tsx
+++ FE/src/App.tsx
@@ -2,17 +2,29 @@ import { Routes, Route, BrowserRouter } from "react-router-dom";
 import TestPages from "./pages/TestPages";
 import Layout from "./components/Layout/Layout";
 
+import Step1ClassificationSetup from "./pages/Step1ClassificationSetup";
+
 function App() {
   return (
-    <>
+    <BrowserRouter>
       <Layout>
-        <BrowserRouter>
-          <Routes>
-            <Route path="/test" element={<TestPages />} />
-          </Routes>
-        </BrowserRouter>
+        <Routes>
+          <Route path="/test" element={<TestPages />} />
+
+          {/* 사용자 모드 */}
+          <Route path="/user">
+            <Route path="step1" element={<Step1ClassificationSetup />} />
+            <Route index element={<Step1ClassificationSetup />} />
+          </Route>
+
+          {/* 관리자 모드 */}
+          <Route path="/admin">
+            <Route path="step1" element={<Step1ClassificationSetup />} />
+            <Route index element={<Step1ClassificationSetup />} />
+          </Route>
+        </Routes>
       </Layout>
-    </>
+    </BrowserRouter>
   );
 }
 
