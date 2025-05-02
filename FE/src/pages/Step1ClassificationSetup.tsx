9f3b3709a2e25af5c3b262be35800ed6a7990147
이송희 <thdgml1103@gmail.com>
2025-05-02 10:47:41 +0900

    Feat: step1 페이지 컴포넌트 개발

---
+++ FE/src/pages/Step1ClassificationSetup.tsx
@@ -0,0 +1,16 @@
+import Title from "../components/common/Title";
+import IntroContent from "../components/Step1/IntroContent";
+
+function Step1ClassificationSetup() {
+  return (
+    <div className="p-8 pb-6 flex flex-col justify-stretch grow">
+      <Title
+        text="분류 체계 준비"
+        subText="특허 분류를 위해 맞춤형 분류 체계를 AI로 생성하거나 기존 체계 파일을 업로드하세요."
+      />
+      <IntroContent />
+    </div>
+  );
+}
+
+export default Step1ClassificationSetup;
