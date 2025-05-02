63e529ae097029a375de53ead78ee5b74081c889
이송희 <thdgml1103@gmail.com>
2025-05-02 11:12:12 +0900

    Feat: step1 페이지에 프롬프트 컴포넌트 추가

--- FE/src/pages/Step1ClassificationSetup.tsx
+++ FE/src/pages/Step1ClassificationSetup.tsx
@@ -1,5 +1,6 @@
 import Title from "../components/common/Title";
 import IntroContent from "../components/Step1/IntroContent";
+import Prompt from "../components/Step1/Prompt";
 
 function Step1ClassificationSetup() {
   return (
@@ -9,6 +10,7 @@ function Step1ClassificationSetup() {
         subText="특허 분류를 위해 맞춤형 분류 체계를 AI로 생성하거나 기존 체계 파일을 업로드하세요."
       />
       <IntroContent />
+      <Prompt />
     </div>
   );
 }
