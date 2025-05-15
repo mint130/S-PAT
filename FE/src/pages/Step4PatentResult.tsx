27f0b5a936e174c3e3c5bfea49d05c08c1d103dc
김은영 <kimeunyoung-0425@naver.com>
2025-05-15 14:30:51 +0900

    Refactor: Step4PatentReesult  컴포넌트에서  불필요한 코드 제거 및 구조 개선 S12P31S108-221

--- FE/src/pages/Step4PatentResult.tsx
+++ FE/src/pages/Step4PatentResult.tsx
@@ -1,76 +1,23 @@
 import { useState } from "react";
-import { useLocation, useNavigate } from "react-router-dom";
+import { useLocation } from "react-router-dom";
 import Title from "../components/common/Title";
+import patentColumnsStep4 from "../components/Step4/patentColumsStep4";
 import DataTable from "../components/common/dataTable/DataTable";
-import Button from "../components/common/Button";
-import type { ColDef } from "ag-grid-community";
 
 function Step4PatentResult() {
   const location = useLocation();
-  const navigate = useNavigate();
-  const [colDefs] = useState<ColDef<any, any>[]>([
-    {
-      headerName: "출원 번호",
-      field: "applicationNumber",
-    },
-    {
-      headerName: "명칭",
-      field: "title",
-    },
-    {
-      headerName: "요약",
-      field: "abstract",
-    },
-    {
-      headerName: "대분류 코드",
-      field: "majorCode",
-    },
-    {
-      headerName: "중분류 코드",
-      field: "middleCode",
-    },
-    {
-      headerName: "소분류 코드",
-      field: "smallCode",
-    },
-    {
-      headerName: "대분류 명칭",
-      field: "majorTitle",
-    },
-    {
-      headerName: "중분류 명칭",
-      field: "middleTitle",
-    },
-    {
-      headerName: "소분류 명칭",
-      field: "smallTitle",
-    },
-  ]);
+  const [colDefs] = useState(patentColumnsStep4);
 
   const { patentResult } = location.state || [];
-  const handlePrevious = () => {
-    navigate("/user/step3");
-  };
 
   return (
-    <div className="flex flex-col h-screen p-8 pb-6">
-      <Title text="특허 데이터분류 결과" subText="" />
+    <div className="flex flex-col h-full p-8">
+      <Title
+        text="특허 데이터 분류 결과"
+        subText="특허 분류 체계를 기준으로 설정된 최적 LLM 모델을 통해 특허 데이터 분류 결과를 확인하실 수 있습니다"
+      />
 
-      <DataTable rowData={patentResult} colDefs={colDefs} />
-
-      {/* 이전/다음 버튼 영역 */}
-      <div className="flex justify-between w-full mt-10">
-        <Button
-          variant="outline"
-          size="md"
-          className="w-24"
-          onClick={handlePrevious}>
-          이전
-        </Button>
-        <Button variant="primary" size="md" className="w-24">
-          종료
-        </Button>
-      </div>
+      <DataTable rowData={patentResult} colDefs={colDefs} download={true} />
     </div>
   );
 }
