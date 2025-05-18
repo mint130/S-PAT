5de8e9eb1b95773d6ccc7e0a36bef7c1b5359d73
제동균 <donggyunje@gmail.com>
2025-05-18 09:47:03 +0900

    Feat : 전문가 특허 분류 시 파일 길이 제공

--- FE/src/pages/Step3PatentClassification.tsx
+++ FE/src/pages/Step3PatentClassification.tsx
@@ -125,7 +125,7 @@ function Step3PatentClassification() {
     <div className="p-8 pb-6 h-full flex flex-col justify-stretch grow">
       {isClassifying ? (
         localStorage.getItem("role") === "Admin" ? (
-          <MultiAILoadingScreen sessionId={sessionId} />
+          <MultiAILoadingScreen sessionId={sessionId} fileLength={fileLength}/>
         ) : (
           <UserLoading sessionId={sessionId} fileLength={fileLength} />
         )
