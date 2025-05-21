ead3a92fd721411d8736df01987165fb0438d247
김민주 <mintminju130@naver.com>
2025-05-21 22:06:51 +0900

    Fix: 시연용 reasoning gemini 모델 교체

--- BE/app/core/llm.py
+++ BE/app/core/llm.py
@@ -17,7 +17,8 @@ grok = ChatXAI(model="grok-3-beta", temperature=0, xai_api_key=settings.GROK3_AP
 gpt_reasoning = ChatOpenAI(api_key=settings.OPENAI_API_KEY, model_name="gpt-4o", temperature=0)
 claude_reasoning = ChatAnthropic(model="claude-3-7-sonnet-20250219", temperature=0, api_key=settings.CLAUDE_API_KEY)
 gemini_reasoning = ChatGoogleGenerativeAI(
-    model="gemini-2.5-pro-preview-03-25",
+    #model="gemini-2.5-pro-preview-03-25",
+    model="gemini-2.0-flash",
     temperature=0,
     google_api_key=settings.GEMINI_API_KEY
 )
